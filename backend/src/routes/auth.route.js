7889aa1ffcc3f8a0a92b960d3383545d9ea7e769
iqratariqq <iqrat269@gmail.com>
2025-11-27 10:28:21 +0500

    added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { login, logout, refreshToken, registerKitchen, signup } from "../controller/auth.controller.js";
 import { isVendor } from "../middelware/auth.middelware.js";
+import upload from "../middelware/multer.middelware.js";
+
 const router = express.Router();
 
 router.post("/signup/:role", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/vendor/register-kitchen",isVendor,registerKitchen);
+router.post("/vendor/register-kitchen",isVendor,upload.single('kitchenImage'),registerKitchen);
 
 // refresh access token
 router.post("/refresh-token", refreshToken);
