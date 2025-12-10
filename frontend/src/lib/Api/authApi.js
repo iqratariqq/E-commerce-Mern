46a4aae02054a6c1bbc24bcd8a14e9362d83a923
iqratariqq <iqrat269@gmail.com>
2025-12-10 17:13:01 +0500

    cors setting added in  vite config file

---
+++ frontend/src/lib/Api/authApi.js
@@ -0,0 +1,24 @@
+import axiosInstance from "../axios"
+
+export const signUp=async(userData)=>{
+    try {
+       const res= await axiosInstance.post(`/auth/signup/${customer}`,userData)
+        return res.data
+        
+    } catch (error) {
+        throw new Error
+    }
+
+}
+
+
+export const login=async(userData)=>{
+    try {
+       const res= await axiosInstance.post(`/auth/login`,userData)
+        return res.data
+        
+    } catch (error) {
+        throw new Error
+    }
+
+}
